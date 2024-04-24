3a2336c8ee1827d11e475be2479af080807160a9
artem nikishin <93470346+ArtemNPRO@users.noreply.github.com>
2024-04-24 18:46:05 +0300

    Add files via upload
    FVJKDFZJDSJI

---
+++ ping-pong.py
@@ -0,0 +1,17 @@
+from pygame import *
+init()
+back = (200,255,255)
+win_width=600
+win_heigth = 500
+window = display.set_mode((win_width, win_heigth))
+window.fill(back)
+game = True
+clock = time.Clock()
+FPS = 60
+while game:
+    for e in event.get():
+        if e.type==QUIT:
+                game = False
+    display.update()
+    clock.tick(FPS)
+
